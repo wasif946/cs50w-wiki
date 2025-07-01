7b5631100938e56f4d622dda2f65af7bd8d4ade6
Wasif Waheed <wasifwaheed71@gmail.com>
2025-07-01 16:10:38 +0500

    Create __init__.py

---
+++ wiki/__init__.py
@@ -0,0 +1,5 @@
+# This file marks the 'encyclopedia' directory as a Python package.
+
+# It also tells Django to use the 'EncyclopediaConfig' class defined in apps.py
+# as the default application configuration for this app.
+default_app_config = 'encyclopedia.apps.EncyclopediaConfig'
